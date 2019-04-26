a77bf1124d5db79791b09008646e5425aa9f8741
ciaran-dk <ciaran@murraynet.dk>
2019-04-26 21:52:41 +0200

    rerun Megrundsområdet

--- run_indicator_calculations.R
+++ run_indicator_calculations.R
@@ -16,28 +16,23 @@ nSimMC <- 1000
 #number of Monte Carlo simulations
 
 
-outputdbC<-"output/ekostat_C.db"
+outputdbC<-"output/ekostat_C_WA86698934.db"
 outputdbL<-"output/ekostat_L.db"
 outputdbR<-"output/ekostat_R.db"
 
 
 wblistC<-df_WB %>% 
   distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="Coast")
+  filter(CLR=="Coast") %>%
+  filter(WB_ID=="WA86698934")
 
-wblistL<-df_WB %>% 
-  distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="Lake") 
-
-wblistR<-df_WB %>% 
-  distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="River") 
 
 #List of periods to be assessed from prepare_input_data.R
 
 # dfc - coastal water measurements
-# dfl - lake measurements
-# dfr - river measurements
+dfc <- dfc %>%
+  filter(WB_ID=="WA86698934") %>%
+  filter(station!="HÖ3 / ÖGERBOFJÄRDEN")
 
 #--------------------------------------------------------------------------------------
 ind<-df_indicators %>% 
@@ -47,9 +42,9 @@ ind<-df_indicators %>%
 ind<-paste0(ind$Indicator)
 
 
-AssessmentMultiple(wblistC,df_periods,dfc,outputdbC,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=F,logfile="log_C.txt",iStart=1)
-AssessmentMultiple(wblistL,df_periods,dfl,outputdbL,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_L.txt")
-AssessmentMultiple(wblistR,df_periods,dfr,outputdbR,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_R.txt")
+AssessmentMultiple(wblistC,df_periods,dfc,outputdbC,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_C_WA86698934.txt",iStart=1)
+#AssessmentMultiple(wblistL,df_periods,dfl,outputdbL,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_L.txt")
+#AssessmentMultiple(wblistR,df_periods,dfr,outputdbR,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_R.txt")
 
 
 
