eca8bc933d5781b001ab60763512797d57ff8c1d
ciaran-dk <ciaran@murraynet.dk>
2019-04-29 14:25:15 +0200

    recalculate BQI after replacing missing station depth values

--- run_indicator_calculations.R
+++ run_indicator_calculations.R
@@ -16,22 +16,19 @@ nSimMC <- 1000
 #number of Monte Carlo simulations
 
 
-outputdbC<-"output/ekostat_C.db"
-outputdbL<-"output/ekostat_L.db"
-outputdbR<-"output/ekostat_R.db"
+outputdbC<-"output/ekostat_C_BQI.db"
+
+
+BQIlist<-c("WA80139110","WA23043276","WA63295529","WA69972288","WA79325467","WA82857164","WA98700208","WA51137015","WA55040263",
+"WA51265873","WA38927215","WA96277013","WA24348954","WA18974073","WA12895460","WA70644789","WA35362374","WA23967996",
+"WA11249942","WA88316659","WA13547710","WA19927154")
 
 
 wblistC<-df_WB %>% 
   distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="Coast")
+  filter(CLR=="Coast") %>%
+  filter(WB_ID %in% BQIlist)
 
-wblistL<-df_WB %>% 
-  distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="Lake") 
-
-wblistR<-df_WB %>% 
-  distinct(CLR,WB_ID,Type) %>%
-  filter(CLR=="River") 
 
 #List of periods to be assessed from prepare_input_data.R
 
@@ -45,11 +42,9 @@ ind<-df_indicators %>%
   filter(!Indicator %in% c("RiverpHchange"))
 
 ind<-paste0(ind$Indicator)
+ind<-c("CoastBQI")
 
-
-AssessmentMultiple(wblistC,df_periods,dfc,outputdbC,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=F,logfile="log_C.txt",iStart=1)
-AssessmentMultiple(wblistL,df_periods,dfl,outputdbL,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_L.txt")
-AssessmentMultiple(wblistR,df_periods,dfr,outputdbR,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_R.txt")
+AssessmentMultiple(wblistC,df_periods,dfc,outputdbC,ind,df_bound,df_bound_WB,df_indicators,df_varcomp,df_var,nSimMC,bReplaceResults=T,logfile="log_C_BQI.txt",iStart=1)
 
 
 
